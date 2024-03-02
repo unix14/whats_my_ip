dadefcb2a3e6900d6145dabc2bdac8226b2eeff8
Eyal Yaakobi <unix14@gmail.com>
2024-03-02 22:04:40 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/whats_my_ip/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.whats_my_ip
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:dart_ipify/dart_ipify.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'What\'s My IP?',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'What\'s My IP?'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String currentIp = "N/A";
+
+  @override
+  void initState() {
+    _calculateIp();
+    super.initState();
+  }
+
+  _calculateIp() async {
+    var ipv4 = await Ipify.ipv4();
+    setState(() {
+      currentIp = ipv4;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //todo add ads here
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.onTertiaryFixed,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Your IP address is:',
+            ),
+            Text(
+              currentIp,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 18),
+              child: IconButton.outlined(
+                onPressed: () {
+                  Clipboard.setData(ClipboardData(text: currentIp));
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text("IP was copied"),
+                  ));
+                },
+                icon: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(Icons.copy),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
